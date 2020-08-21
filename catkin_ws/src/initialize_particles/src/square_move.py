79a12de64778e50f4a3b7a203b860b90eaeaabfc
kinseyreeves <kinseyr1313@gmail.com>
2020-08-21 15:19:46 +1000

    test

--- catkin_ws/src/initialize_particles/src/square_move.py
+++ catkin_ws/src/initialize_particles/src/square_move.py
@@ -39,6 +39,7 @@ class SquareMove:
         self.run()
 
     def run(self):
+        
         while self.covariance > 0.65:
             self.disperse()
             self.square_move()
@@ -51,6 +52,7 @@ class SquareMove:
         pass
 
     def calc_covariance(self):
+
         if(len(self.covariance_arr)>0):
             cov_x = self.covariance_arr[0]
             cov_y = self.covariance_arr[7]
@@ -62,17 +64,20 @@ class SquareMove:
         return cov
 
     def disperse(self):
+
         rospy.wait_for_service('/global_localization')
         client = rospy.ServiceProxy('/global_localization', Empty)
         req_object = EmptyRequest()
         result = client(req_object)
 
     def sub_pose_callback(self, msg):
+
         #print("getting pose")
         self.pose = msg.pose
         self.covariance_arr = msg.pose.covariance
 
     def square_move(self):
+
         def move_forward():
             print("moving foward")
             i= 0
@@ -84,6 +89,7 @@ class SquareMove:
                 self.rate.sleep()
             
         def turn_left(linear_vel = 0.0, angular_vel=0.8):
+
             print("moving left")
             i= 0
             while i < 10:
@@ -96,14 +102,7 @@ class SquareMove:
         for i in range(0,4):
             move_forward()
             turn_left()
-        
-
         return
-
-
-    def print_covar(self):
-        pass
-
 
 if __name__ == '__main__':
     rospy.init_node("SquareMove")
